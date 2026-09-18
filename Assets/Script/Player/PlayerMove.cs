bf0301d8b5ea3fd0eb3bef8c531456abbb738777
Lunorsun <srobot0141@gmail.com>
2026-09-18 13:36:12 +0900

    마우스 포인터 방향 플레이어 공격방향으로
    마우스 포인터 방향을
    플레이어가 공격하는 방향으로 바꿈

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -22,13 +22,12 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        //wasd로 이동, player_speed변수로 속도 조절
+        // WASD 이동 입력
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
-        
 
-        //움직일 때 애니메이션 작동
-        if((moveX!=0||moveY!=0) && animator.GetInteger("Attack")==-1 && animator.GetInteger("Attacked")==-1 && moveable)
+        // 움직일 때 애니메이션 작동
+        if ((moveX != 0 || moveY != 0) && animator.GetInteger("Attack") == -1 && animator.GetInteger("Attacked") == -1 && moveable)
         {
             animator.SetBool("Run", true);
         }
@@ -36,24 +35,35 @@ public class PlayerMove : MonoBehaviour
         {
             animator.SetBool("Run", false);
         }
-        //방향전환
-        if((moveX>0) && animator.GetInteger("Attack")==-1 && animator.GetInteger("Attacked")==-1 && moveable)
+
+        // --- [마우스 방향 전환 로직 시작] ---
+        if (moveable && animator.GetInteger("Attack") == -1 && animator.GetInteger("Attacked") == -1)
         {
-            player_transform.localScale = new Vector3(-1, 1, 1);
-            player_direction = 0;
+            // 1. 화면의 마우스 좌표를 월드 게임 좌표로 변환
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // 2. 마우스 x위치가 캐릭터 x위치보다 오른쪽에 있는지 비교
+            if (mousePosition.x > transform.position.x)
+            {
+                // 오른쪽 바라보기 (기존 스프라이트 원본이 오른쪽을 바라볼 경우: -1 또는 1 적용)
+                player_transform.localScale = new Vector3(-1, 1, 1);
+                player_direction = 0;
+            }
+            else if (mousePosition.x < transform.position.x)
+            {
+                // 왼쪽 바라보기
+                player_transform.localScale = new Vector3(1, 1, 1);
+                player_direction = 1;
+            }
         }
-        else if((moveX<0) && animator.GetInteger("Attack")==-1 && animator.GetInteger("Attacked")==-1 && moveable)
-        {
-            player_transform.localScale = new Vector3(1, 1, 1);
-            player_direction = 1;
-        }
+        // --- [마우스 방향 전환 로직 끝] ---
     }
 
     void FixedUpdate()
     {
-        if(moveable)
-            rigid.velocity = new Vector3(moveX, moveY, 0)*GameManager.player_speed;
+        if (moveable)
+            rigid.velocity = new Vector2(moveX, moveY).normalized * GameManager.player_speed;
         else
-            rigid.velocity = Vector3.zero;
+            rigid.velocity = Vector2.zero;
     }
 }
